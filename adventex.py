dc544becee0ae6c150e0bb44dfd2aa9272630f5e
harry-cramp <harrycramp@hotmail.com>
2021-02-06 12:47:05 +0000

    Implement internal variable queries

--- adventex.py
+++ adventex.py
@@ -7,6 +7,9 @@ VARIABLE_LABEL_VALUE = "value"
 def query_variable(query):
     return input(query)
 
+def remove_quotes(text):
+    return text[1:-2]
+
 def process_variables(lines):
     for line in lines:
         if line[0] != '\t':
@@ -14,16 +17,21 @@ def process_variables(lines):
         words = line.split()
         variable_id = words[0]
         variable_value = line.partition(' ')[2]
-        variable_store[variable_id] = variable_value[1:-2]
+        components = variable_value.split('=')
+        label = components[0]
+        if label == VARIABLE_LABEL_QUERY:
+            variable_store[variable_id] = query_variable(remove_quotes(variable_value.split('=')[1]))
+        elif label == VARIABLE_LABEL_VALUE:
+            variable_store[variable_id] = remove_quotes(components[1])
 
 def printvf(text):
     formatted_text = ""
     for word in text.split():
         if word[0] == '%':
             key = word[1:]
-            word = " " + variable_store[key]
+            word = variable_store[key] + " "
         else:
-            word = " " + word
+            word = word + " "
         formatted_text += word
     print(formatted_text)
 
@@ -32,11 +40,9 @@ data = game_file.readlines()
 index = 0
 for line in data:
     line = line.strip()
-    print(line)
     if line == "TITLE":
         print(line.split()[1])
     elif line == "VARIABLES":
-        print("LOADING VARIABLES")
         process_variables(data[index+1:])
     elif line == "GAME":
         break
@@ -44,4 +50,5 @@ for line in data:
 # process situations
 # process ending
 
+printvf("Your name is %NAME and you have %LIVES lives.")
     
